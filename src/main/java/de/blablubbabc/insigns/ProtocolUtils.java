72b0fa94b8d1fef4f755ae354fc9aeea198f0519
blablubbabc <lukas@wirsindwir.de>
2016-12-22 14:27:43 +0100

    Updated to 1.11+
    The tile entity id for signs was changed.

--- src/main/java/de/blablubbabc/insigns/ProtocolUtils.java
+++ src/main/java/de/blablubbabc/insigns/ProtocolUtils.java
@@ -111,7 +111,9 @@ public class ProtocolUtils {
 			}
 
 			public static boolean isTileEntitySignData(NbtCompound tileEntityData) {
-				return getId(tileEntityData).equals("Sign");
+				String id = getId(tileEntityData);
+				// pre 1.11 and 1.11+ ids:
+				return id.equals("Sign") || id.equals("minecraft:sign");
 			}
 
 			public static String[] getText(NbtCompound tileEntitySignData) {
